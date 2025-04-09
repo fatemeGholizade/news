e07afc6b4bfd611668e59ec67837c4dd61949c03
gholizadefatemeh37 <gholizadefatemeh37@gmail.com>
2025-04-09 10:58:43 +0330

    add next image

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,13 +1,22 @@
 'use client'
 import { useSelector } from "react-redux";
 import { RootState } from "app/types/store";
-import  styles  from "app/style/page.module.scss";
+import  styles  from "app/[id]/style/page.module.scss";
+import Image from "next/image";
 export default function IndividualNews() {
   const selectedArticle = useSelector((state: RootState) => state.article.selectedArticle);
     return (
       <div className={styles.wrapper}>
-        <img className={styles.image} width={600} height={500} loading="lazy" src={selectedArticle?.urlToImage || ""} alt="img" />
-        <h1>Title: {selectedArticle?.title}</h1>
+{selectedArticle?.urlToImage !== null && selectedArticle?.urlToImage !== undefined && (
+        <Image
+          width="290"
+          height="200"
+          className={styles.image}
+          loading="lazy"
+          src={selectedArticle?.urlToImage}
+          alt={"image"}
+        />
+      )}        <h1>Title: {selectedArticle?.title}</h1>
         <h2>Author: {selectedArticle?.author}</h2>
         <p>description: {selectedArticle?.description}</p>
       </div>
